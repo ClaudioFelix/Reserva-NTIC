6f270b64b2d940eff4c525727178c43283f0eff3
LeafarTHENERD <leafar.thenerd@hotmail.com>
2013-11-01 09:24:59 -0200

    Classe de Entidade de Tipo de Recurso

---
+++ src/br/cefetmg/sinapse/ntic/nucleo/dominio/TipoRecurso.java
@@ -0,0 +1,73 @@
+package br.cefetmg.sinapse.ntic.nucleo.dominio;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: TipoRecurso
+ *
+ */
+@Entity
+@NamedQueries( {
+    @NamedQuery(name = "TipoRecurso.findAll", query = "SELECT s FROM TipoRecurso s"),
+    @NamedQuery(name = "TipoRecurso.findByNome", query = "SELECT s FROM TipoRecurso s WHERE s.nomeTipoRecurso = :nomeTipoRecurso"),
+    @NamedQuery(name = "TipoRecurso.findByLikeNome", query = "SELECT s FROM TipoRecurso s WHERE upper(s.nomeTipoRecurso) like :nomeTipoRecurso"),
+    @NamedQuery(name = "TipoRecurso.findByCodigoTipoRecurso", query = "SELECT s FROM TipoRecurso s WHERE s.CodigoTipoRecurso = :CodigoTipoRecurso")
+})
+
+public class TipoRecurso{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigoTipoRecurso = 0;
+	
+	private String nomeTipoRecurso;
+	
+	/* private Funcionario responsavel */
+	// Abstração
+	private List<Recurso> lista_recursos;
+	
+	public TipoRecurso() {
+		super();
+	}
+	
+	public long getCodigoTipoRecurso() {
+		return codigoTipoRecurso;
+	}
+
+	public void setCodigoTipoRecurso(long codigoTipoRecurso) {
+		this.codigoTipoRecurso = codigoTipoRecurso;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoRecurso [codigoTipoRecurso=" + codigoTipoRecurso + ", nomeTipoRecurso=" + nomeTipoRecurso + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + funcional;
+		result = prime * result + (int) (codigoTipoRecurso ^ (codigoTipoRecurso >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TipoRecurso other = (TipoRecurso) obj;
+		if (codigoTipoRecurso != other.codigoTipoRecurso)
+			return false;
+		if (nomeTipoRecurso != other.nomeTipoRecurso)
+			return false;
+		return true;
+	}   
+}
