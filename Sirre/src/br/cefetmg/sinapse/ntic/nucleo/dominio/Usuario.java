f995a2fe93dc4bb589bef7592520f40752a693ef
LeafarTHENERD <leafar.thenerd@hotmail.com>
2013-12-26 20:41:16 -0200

    Apenas uma classe representativa para demonstrar o desejo do mapeamento dos usuários no Sistema

---
+++ Sirre/src/br/cefetmg/sinapse/ntic/nucleo/dominio/Usuario.java
@@ -0,0 +1,24 @@
+package br.cefetmg.sinapse.ntic.nucleo.dominio;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Usuario
+ *
+ */
+@Entity
+
+public class Usuario implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private long idUsuario;	
+
+	public Usuario() {
+		super();
+	}
+   
+}
